6cdfe264ab49ba602dbc004689319c4e05c0f3e4
Reia-O <ewokakunogasuki@gmail.com>
2024-11-14 17:19:23 +0900

    fix code

--- テンプレ/timer.py
+++ テンプレ/timer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import datetime
-import time
 import RPi.GPIO as GPIO
 
 # 変数宣言
@@ -12,24 +11,24 @@ LED_active = True
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_GPIO , GPIO.OUT)
 
-def main():
-    try:
-        while True:
-        # 朝6時にLEDを点灯
-            if LED_active == False and datetime.datetime.now().strftime("%H:%M") == "06:00":
-                print("LED_on_" + datetime.datetime.now().strftime("%H:%M"))
-                GPIO.output(LED_GPIO, GPIO , True)
-                LED_active = True
 
-            # 夜21時にLEDを消灯
-            if LED_active == True and datetime.datetime.now().strftime("%H:%M") == "21:00":
-                print("LED_off_" + datetime.datetime.now().strftime("%H:%M"))
-                GPIO.output(LED_GPIO, GPIO , False)
-                LED_active = False
+try:
+    while True:
+    # 朝6時にLEDを点灯
+        if LED_active == False and datetime.datetime.now().strftime("%H:%M") == "15:44":
+            print("LED_on_" + datetime.datetime.now().strftime("%H:%M"))
+            GPIO.output(LED_GPIO, 1)
+            LED_active = True
 
-    except Exception as err_txt:
-        #print("----- Error! -----")
-        pass
-    finally:
-        #クリーンナップ#
-        GPIO.cleanup()
+        # 夜21時にLEDを消灯
+        if LED_active == True and datetime.datetime.now().strftime("%H:%M") == "15:45":
+            print("LED_off_" + datetime.datetime.now().strftime("%H:%M"))
+            GPIO.output(LED_GPIO, 0)
+            LED_active = False
+
+except Exception as err_txt:
+    #print("----- Error! -----")
+    pass
+finally:
+    #クリーンナップ#
+    GPIO.cleanup()
